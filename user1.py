aa9a94b0799c7e02f93257934942ab6f1e420f0c
Nikhil Kumar <nikhilkumar654231@gmail.com>
2020-09-12 12:26:27 +0530

    [FIX]Fixed result declaration

--- user1.py
+++ user1.py
@@ -3,7 +3,7 @@ import socket
 
 gamesocket= socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 b = []
-label = None
+
 
 class Game():
     def __init__(self):
@@ -12,51 +12,22 @@ class Game():
         self.color = {0:"deep sky blue", 1:"lawn green"}
         self.move_count = 0
         self.labels = ["You move", "Awaiting move from network"]
-    
-    def draw(self):
+
+        tk = tkinter.Tk()
+        tk.title("Tic Tac Toe")
         c = 0
-        for i in range(0, 3):
-            for j in range(0, 3):
+        
+        for i in range(3):
+            for j in range(3):
+                b.append(self.button(tk))
+                b[c].config(command = lambda box=c:self.click(box))
+                b[c].grid(row=i, column=j)
                 c += 1
-                if self.stat[c] is not None:
-                    print("| "+self.symbols[self.stat[c]], end=" |")
-                else:
-                    print("|  ", end=" |")
-            print("\n--------------------")
-    
-    def loop(self):
-        while True:
-            self.draw()
-            data, client_address = gamesocket.recvfrom(1024)
-            data = data.decode('utf-8')
-            self.stat[int(data)]=0
-            x = self.check_win()
-            if x is 1:
-                print("The winner is You")
-                print("Closing Connection")
-                exit(0)
-            elif x is 0:
-                print("The winner is Not You")
-                print("Closing Connection")
-                exit(0)
-            self.draw()
-            new = int(input("Enter next move\n"))
-            while True:
-                if self.stat[new] is None:
-                    self.stat[new]=1
-                    gamesocket.sendto(str(new).encode('utf-8'),(socket.gethostname(),8001))
-                    break
-                else:
-                    new = int(input("Enter a valid move\n"))
-            x = self.check_win()
-            if x is 1:
-                print("The winner is You")
-                print("Closing Connection")
-                exit(0)
-            elif x is 0:
-                print("The winner is Not You")
-                print("Closing Connection")
-                exit(0)
+        
+        self.label = tkinter.Label(text=self.labels[1], font=('arial', 20, 'bold'))
+        self.label.grid(row=3, column=0, columnspan=3)
+        tk.mainloop()
+        self.update()
 
     def button(self, frame):          
         b=tkinter.Button(frame,padx=1,bg="papaya whip",width=3,text="   ",font=('arial',60,'bold'),relief="sunken",bd=10)
@@ -69,7 +40,7 @@ class Game():
         gamesocket.sendto(str(box).encode('utf-8'),(socket.gethostname(),8001))
         self.check_win()
         print(self.stat)
-        label.config(text=self.labels[1],  font=('arial', 20, 'bold'))
+        self.label.config(text=self.labels[1],  font=('arial', 20, 'bold'))
         self.update()
 
     def update(self):
@@ -78,24 +49,9 @@ class Game():
         print(int(data))
         self.move_count += 1
         b[int(data)].config(text='O', state=tkinter.DISABLED, disabledforeground=self.color[1])
-        label.config(text=self.labels[0],  font=('arial', 20, 'bold'))
+        self.label.config(text=self.labels[0],  font=('arial', 20, 'bold'))
         self.check_win()
         return
-
-    def gui(self):
-        tk = tkinter.Tk()
-        tk.title("Tic Tac Toe")
-        c = 0
-        for i in range(3):
-            for j in range(3):
-                b.append(self.button(tk))
-                b[c].config(command = lambda box=c:self.click(box))
-                b[c].grid(row=i, column=j)
-                c += 1
-        label = tkinter.Label(text=self.labels[1], font=('arial', 20, 'bold'))
-        label.grid(row=3, column=0, columnspan=3)
-        tk.mainloop()
-        self.update()
 
     def check_win(self):
         x = None
@@ -113,7 +69,7 @@ class Game():
                 x =  self.stat[1]
             if self.stat[2] == self.stat[5] and self.stat[2] == self.stat[8]:
                 x =  self.stat[2]
-            if self.stat[3] == self.stat[5] and self.stat[3] == self.stat[9]:
+            if self.stat[3] == self.stat[6] and self.stat[3] == self.stat[9]:
                 x =  self.stat[3]
             if self.stat[1] == self.stat[5] and self.stat[1] == self.stat[9]:
                 x =  self.stat[1]
@@ -131,4 +87,3 @@ if __name__ == '__main__':
     gamesocket.bind((socket.gethostname(),8000))
     print("game running on :",socket.gethostname())
     game = Game()
-    game.gui()
--- user2.py
+++ user2.py
@@ -3,7 +3,6 @@ import tkinter
 
 gamesocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 b = []
-label = None
 
 class Game():
     def __init__(self):
@@ -12,52 +11,21 @@ class Game():
         self.color = {0:"deep sky blue", 1:"lawn green"}
         self.move_count = 0
         self.labels = ["You move", "Awaiting move from network"]
-    
-    def draw(self):
+        
+        tk = tkinter.Tk()
+        tk.title("Tic Tac Toe")
         c = 0
-        for i in range(0, 3):
-            for j in range(0, 3):
+        
+        for i in range(3):
+            for j in range(3):
+                b.append(self.button(tk))
+                b[c].config(command = lambda box=c:self.click(box))
+                b[c].grid(row=i, column=j)
                 c += 1
-                if self.stat[c] is not None:
-                    print("| "+self.symbols[self.stat[c]], end=" |")
-                else:
-                    print("|  ", end=" |")
-            print("\n--------------------")
-    
-    def loop(self):
-        while True:
-            x = self.check_win()
-            if x is 1:
-                print("The winner is Not You")
-                print("Closing Connection")
-                exit(0)
-            elif x is 0:
-                print("The winner is You")
-                print("Closing Connection")
-                exit(0)
-            new = int(input("Enter next move\n"))
-            while True:
-                if self.stat[new] is None:
-                    self.stat[new]=0
-                    gamesocket.sendto(str(new).encode('utf-8'),(socket.gethostname(),8000))
-                    break
-                else:
-                    new = int(input("Enter a valid move\n"))
-            self.draw()
-            x = self.check_win()
-            if x is 1:
-                print("The winner is Not You")
-                print("Closing Connection")
-                exit(0)
-            elif x is 0:
-                print("The winner is You")
-                print("Closing Connection")
-                exit(0)
-            data, client_address = gamesocket.recvfrom(1024)
-            data = data.decode('utf-8')
-            self.stat[int(data)] = 1
-            self.draw()
-            
+        
+        self.label = tkinter.Label(text=self.labels[0], font=('arial', 20, 'bold'))
+        self.label.grid(row=3, column=0, columnspan=3)
+        tk.mainloop()
 
     def button(self, frame):          
         b=tkinter.Button(frame,padx=1,bg="papaya whip",width=3,text="   ",font=('arial',60,'bold'),relief="sunken",bd=10)
@@ -68,8 +36,7 @@ class Game():
         self.stat[box+1] = 0
         self.move_count += 1
         gamesocket.sendto(str(box).encode('utf-8'),(socket.gethostname(),8000))
-        self.check_win()
-        label.config(text=self.labels[1],  font=('arial', 20, 'bold'))
+        self.label.config(text=self.labels[1],  font=('arial', 20, 'bold'))
         self.update()
         
 
@@ -78,23 +45,8 @@ class Game():
         self.stat[int(data)+1] = 1
         self.move_count += 1
         b[int(data)].config(text='X', state=tkinter.DISABLED, disabledforeground=self.color[1])
-        label.config(text=self.labels[0],  font=('arial', 20, 'bold'))
+        self.label.config(text=self.labels[0],  font=('arial', 20, 'bold'))
         self.check_win()
-        return
-
-    def gui(self):
-        tk = tkinter.Tk()
-        tk.title("Tic Tac Toe")
-        c = 0
-        for i in range(3):
-            for j in range(3):
-                b.append(self.button(tk))
-                b[c].config(command = lambda box=c:self.click(box))
-                b[c].grid(row=i, column=j)
-                c += 1
-        label = tkinter.Label(text=self.labels[0], font=('arial', 20, 'bold'))
-        label.grid(row=3, column=0, columnspan=3)
-        tk.mainloop()
 
     def check_win(self):
         x = None
@@ -112,7 +64,7 @@ class Game():
                 x =  self.stat[1]
             if self.stat[2] == self.stat[5] and self.stat[2] == self.stat[8]:
                 x =  self.stat[2]
-            if self.stat[3] == self.stat[5] and self.stat[3] == self.stat[9]:
+            if self.stat[3] == self.stat[6] and self.stat[3] == self.stat[9]:
                 x =  self.stat[3]
             if self.stat[1] == self.stat[5] and self.stat[1] == self.stat[9]:
                 x =  self.stat[1]
@@ -129,5 +81,4 @@ class Game():
 if __name__ == '__main__':
     gamesocket.bind((socket.gethostname(),8001))
     print("game running on :",socket.gethostname())
-    game= Game()
-    game.gui()
+    game = Game()

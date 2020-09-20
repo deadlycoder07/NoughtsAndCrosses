377d1aeb0000732d85f982834712ceaff9589576
Nikhil Kumar <nikhilkumar654231@gmail.com>
2020-09-21 00:46:28 +0530

    [ADD] Req files

---
+++ mclient.py
@@ -0,0 +1,30 @@
+import socket 
+
+
+def Main(): 
+	
+	host = '127.0.0.1'
+	port = 12345
+
+	s = socket.socket(socket.AF_INET,socket.SOCK_STREAM) 
+	s.connect((host,port)) 
+
+	 
+	message = "The WORLD is Vulnerable and we are going to patch it, NO matter whatever it TAKES!!"
+	while True: 
+		
+		s.send(message.encode('ascii')) 
+        data = s.recv(1024) 
+        print('Received from the server :',str(data.decode('ascii'))) 
+
+		# ask the client whether he wants to continue 
+        ans = input('\nDo you want to continue(y/n) :') 
+        if ans == 'y': 
+			continue
+        else: 
+			break
+	
+	s.close() 
+
+if __name__ == '__main__': 
+	Main() 
